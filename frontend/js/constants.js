b46d836396a250a45d3950d1274de8502a7dea8c
nionanov <nikolayionanov@yahoo.com>
2022-10-24 16:16:15 +0100

    done magic

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,7 +1,7 @@
-const contractAddress = "0x315BBFa2b4C0373aFfDf7c9e612cC50fc8E5E14D"; // Replace with your own contract address
-const chain = 'goerli'; // rinkeby, polygon, or ethereum
+const contractAddress = '0x315BBFa2b4C0373aFfDf7c9e612cC50fc8E5E14D'; // Replace with your own contract address
+const chain = 'goerli'; // goerli, polygon, or ethereum
 
-const welcome_h1 = "Fancy capybaras!!";
+const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
 const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
